40e77198f2e5ac07bc0d0a4ca53b0a311dc3bd18
r01otvm0 <r01otvm0@addy.to>
2026-02-08 16:58:33 -0800

    ...

--- lists/add_movie.sh
+++ lists/add_movie.sh
@@ -5,4 +5,4 @@ set -Eeuxo pipefail
 temp_file=$(mktemp)
 
 jq "if map(select(._title == \"${1}\")) | length == 0 then . + [{\"_title\": \"${1}\"}] end" movies.json > "${temp_file}"
-jq 'sort_by(..)' "${temp_file}" > movies.json
+mv "${temp_file}" movies.json
